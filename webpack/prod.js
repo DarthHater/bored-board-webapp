a79e6b52451cb1fe126f08f2f1759fc601996248
DarthHater <jeffryxtron@gmail.com>
2018-06-26 00:37:16 -0800

    Uglify no likey ES6

--- webpack/prod.js
+++ webpack/prod.js
@@ -3,9 +3,6 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./common.js');
 
 module.exports = merge(common, {
-    plugins: [
-        new UglifyJSPlugin()
-    ],
     devServer: {
         public: 'vivalavinyl-webapp.herokuapp.com',
         contentBase: './dist'
