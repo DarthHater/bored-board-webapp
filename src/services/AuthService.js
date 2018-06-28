0939d5085884fcef1622d1ad3fe8c0c627f1a633
Jeffry Hesse <jhesse@sonatype.com>
2018-06-27 16:12:44 -0800

    Abstract that

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,6 +1,9 @@
+import config from 'react-global-configuration';
+
 class AuthService {
     login(data) {
-        return fetch('http://localhost:8000/login', {
+        let baseUrl = config.get('API_ROOT');
+        return fetch(`${baseUrl}/login`, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
--- src/services/ThreadService.js
+++ src/services/ThreadService.js
@@ -1,11 +1,18 @@
+import config from 'react-global-configuration';
+
 class ThreadService {
+    // Should likely do this in a global config class
+    getBaseUrl() {
+        return config.get('API_ROOT');
+    }
+
     requestHeaders() {
         return {'AUTHORIZATION': `Bearer ${sessionStorage.jwt}`}
     }
 
     getAllThreads() {
         const headers = this.requestHeaders();
-        const request = new Request(`http://localhost:8000/threads`, {
+        const request = new Request(`${getBaseUrl()}/threads`, {
             method: 'GET',
             headers: headers
         });
@@ -21,7 +28,7 @@ class ThreadService {
 
     getThread(threadId) {
         const headers = this.requestHeaders();
-        const request = new Request(`http://localhost:8000/thread/${threadId}`, {
+        const request = new Request(`${getBaseUrl()}/thread/${threadId}`, {
             method: 'GET',
             headers: headers
         });
@@ -37,7 +44,7 @@ class ThreadService {
 
     getPosts(threadId) {
         const headers = this.requestHeaders();
-        const request = new Request(`http://localhost:8000/posts/${threadId}`, {
+        const request = new Request(`${getBaseUrl()}/posts/${threadId}`, {
             method: 'GET',
             headers: headers
         });
@@ -53,7 +60,7 @@ class ThreadService {
 
     postPost(threadId, userId, body) {
         const headers = this.requestHeaders();
-        const request = new Request('http://localhost:8000/post', {
+        const request = new Request(`${getBaseUrl()}/post`, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
