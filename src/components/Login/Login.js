ca46b16849dfd7320a21c5dd8058025d99121abd
DarthHater <jeffryxtron@gmail.com>
2018-07-08 14:07:10 -0800

    Switch from FormData for Safari

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,8 +10,13 @@ class Login extends Component {
         super(props);
 
         this.state = {
-            submitted: false
+            submitted: false,
+            username: '',
+            password: ''
         };
+
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
     }
 
     handleSubmit = event => {
@@ -19,16 +24,22 @@ class Login extends Component {
 
         this.setState({submitted: true});
 
-        const data = new FormData(event.target);
-
         let body = JSON.stringify({
-            username: data.get('username'),
-            password: data.get('password')
+            username: this.state.username,
+            password: this.state.password
         });
 
         const {dispatch} = this.props;
 
         dispatch(userActions.login(body));
+    };
+
+    handleEmailChange = event => {
+        this.setState({username: event.target.value});
+    };
+
+    handlePasswordChange = event => {
+        this.setState({password: event.target.value});
     };
 
     render() {
@@ -37,13 +48,19 @@ class Login extends Component {
                 <h3>Welcome to VLV!</h3>
                 <p> Enter your credentials to login and start shit posting </p>
                 <form onSubmit={this.handleSubmit}>
-                    <TextField hintText="Enter your Username" floatingLabelText="Username" name="username" />
+                    <TextField 
+                        hintText="Enter your Username" 
+                        floatingLabelText="Username" 
+                        name="username" 
+                        onChange={this.handleEmailChange} 
+                    />
                     <br />
                     <TextField
                         type="password"
                         hintText="Enter your Password"
                         floatingLabelText="Password"
                         name="password"
+                        onChange={this.handlePasswordChange}
                     />
                     <br />
                     <RaisedButton label="Login" primary={true} type="submit" />
