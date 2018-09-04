41408e10bc925e407659640eec10c83b982919c9
Jeffry Hesse <jhesse@sonatype.com>
2018-09-04 12:14:06 -0800

    No need for ReactDOM

--- src/components/Thread/Thread.js
+++ src/components/Thread/Thread.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import Timestamp from 'react-timestamp';
 import WebSocket from 'react-websocket';
