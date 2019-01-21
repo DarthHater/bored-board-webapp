c7f1033bddcbc0625665bc5a6d0582b4b7ab5069
Andy <andy.s.bolton@gmail.com>
2019-01-21 14:19:53 -0700

    Add parser for youtube bbcode tag (#44)

---
+++ src/components/Common/BbCode/YouTubeTag.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Tag } from 'bbcode-to-react';
+import { getLocation } from '../../../utilities/utilities';
+
+class YoutubeTag extends Tag {
+    toReact() {
+        let href = this.getContent(true);
+
+        let { host } = getLocation(href);
+
+        // Parse regular and shortened youtube URLs into the  youtube.com/embed/####### style so they work in browser and mobile
+        if (host === "youtube.com" || host === "www.youtube.com" || host === "youtu.be") {
+            if (host === "youtu.be") {
+                href = href.replace("youtu.be/", "youtube.com/embed/");
+            } else {
+                href = href.replace("watch?v=", "embed/");
+            }
+            // disable autoplay
+            href = href.replace("autoplay=", "no=");
+        }
+
+        const attributes = {
+            src: href,
+            width: this.params.width || 420,
+            height: this.params.height || 315,
+        };
+        return (
+            <iframe
+                {...attributes}
+                frameBorder="0"
+                allowFullScreen
+            />
+        );
+    }
+}
+
+export default YoutubeTag;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import config from './config'
 import store, { history } from './store/index';
-import { threadActions } from './actions/threadActions';
 import './App.scss';
-
+import YoutubeTag from './components/Common/BbCode/YouTubeTag';
 import App from './App';
+import parser from 'bbcode-to-react';
+import config from './config'
 
 window.store = store;
+
+parser.registerTag('youtube', YoutubeTag);
 
 render(
     <Provider store={store}>
