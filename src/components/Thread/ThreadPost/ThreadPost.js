15ee8567f566f4e70a83198c4c987ce30dcc2eab
Jeffry Hesse <jhesse@sonatype.com>
2019-01-22 14:02:59 -0900

    Tweak stuff a bit

--- src/components/Thread/ThreadPost/ThreadPost.js
+++ src/components/Thread/ThreadPost/ThreadPost.js
@@ -3,10 +3,23 @@ import { Link } from 'react-router-dom';
 import Timestamp from 'react-timestamp';
 import WebSocket from 'react-websocket';
 import { connect } from 'react-redux';
-import { threadActions } from '../../../actions/index';
 import config from 'react-global-configuration';
 import parser from 'bbcode-to-react';
 import { Grid, Button } from '@material-ui/core';
+import grey from '@material-ui/core/colors/grey';
+
+import { threadActions } from '../../../actions/index';
+import * as auth from '../../../auth/authentication';
+import * as permissions from '../../../constants/permissions';
+
+const styles = {
+    button: {
+        background: grey[400],
+        marginTop: "15px",
+        marginBottom: "15px",
+        marginRight: "15px"
+    }
+};
 
 class ThreadPost extends Component {
 
@@ -14,7 +27,8 @@ class ThreadPost extends Component {
         super(props);
 
         this.state = {
-            baseUrl: config.get('WS_ROOT')
+            baseUrl: config.get('WS_ROOT'),
+            userCanEditPost: auth.userHasPermission(permissions.canEditPost)
         }
 
         this.props.dispatch(threadActions.loadPosts(this.props.threadId));
@@ -28,10 +42,14 @@ class ThreadPost extends Component {
     }
 
     handleQuote = id => {
-        const body = document.getElementById(`post_body_${id}`).innerText;
+        const body = document.getElementById(`post_body_input_${id}`).value;
         const header = document.getElementById(`post_info_${id}`).innerText;
         const post = `[quote]${header}${body}[/quote]`;
         this.props.quotePostCallback(post);
+    }
+
+    editPost = id => {
+        alert(id);
     }
 
     render() {
@@ -40,7 +58,7 @@ class ThreadPost extends Component {
                 {this.props.posts.map(post => {
                     return (
                         <Grid container spacing={0} key={post.Id}>
-                            <Grid item xs={12} sm={6} className="post header info" id={`post_info_${post.Id}`}>
+                            <Grid item xs={12} sm={8} className="post header info" id={`post_info_${post.Id}`}>
                                 <h4>
                                     Posted by <Link to={`/user/${post.UserId}`}>
                                         {post.UserName}
@@ -48,16 +66,28 @@ class ThreadPost extends Component {
                                     &nbsp;on <Timestamp time={post.PostedAt} format="full" />
                                 </h4>
                             </Grid>
-                            <Grid item xs={12} sm={6} className="post header controls">
+                            <Grid item xs={12} sm={4} className="post header controls" >
                                 <Button 
+                                    style={styles.button}
+                                    size="small"
                                     onClick={() => this.handleQuote(post.Id)}
                                 >
                                     » Quote
                                 </Button>
+                                {this.state.userCanEditPost && (
+                                    <Button
+                                        style={styles.button}
+                                        size="small"
+                                        onClick={() => this.editPost(post.Id)}
+                                    >
+                                        » Edit Post
+                                    </Button>
+                                )}
                             </Grid> 
                             <Grid item xs={12} className="post body" id={`post_body_${post.Id}`}>
                                 {parser.toReact(post.Body)}
                             </Grid>
+                            <input type="hidden" value={post.Body} id={`post_body_input_${post.Id}`} />
                         </Grid>
                         
                     )
--- src/constants/permissions.js
+++ src/constants/permissions.js
@@ -3,3 +3,4 @@ import roles from './roles';
 export const deleteThread = [roles.ADMIN, roles.MOD];
 export const deletePost = [roles.ADMIN, roles.MOD];
 export const canPost = [roles.ADMIN, roles.MOD, roles.ELITE, roles.USER];
+export const canEditPost = [roles.ADMIN, roles.MOD];
