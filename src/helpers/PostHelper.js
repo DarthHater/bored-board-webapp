c46efd47f3ba0d14ecddd453939a67cc9547f239
Andy <andy.s.bolton@gmail.com>
2019-01-23 15:35:01 -0700

    Missing import

--- src/helpers/PostHelper.js
+++ src/helpers/PostHelper.js
@@ -1,3 +1,5 @@
+import config from 'react-global-configuration';
+
 export function canEditPost(post) {
     let milliseconds = new Date().getTime() - new Date(post.PostedAt).getTime();
     return (milliseconds / 60000) < config.get('EDIT_POST_TIME');
